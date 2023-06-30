0c87a5c3f812d30405afe8432ab93c148d3472d8
FabricioSobrinho <fabriciomails3@gmail.com>
2023-06-30 10:16:35 -0400

    ...

--- portfolio/src/modules/layouts/ApiPage.jsx
+++ portfolio/src/modules/layouts/ApiPage.jsx
@@ -1,19 +1,25 @@
-import styles from './ApiPage.module.css'
-import cashImage from '../images/cashimage.svg'
+import styles from "./ApiPage.module.css"
+import cashImage from "../images/cashimage.svg"
 
 function ApiPage() {
   return (
     <div className={styles.mainApi}>
-        <div className={styles.leftPage}>
-            <img src={cashImage} alt='Cash img'/>
+      <div className={styles.leftPage}>
+        <img src={cashImage} alt="Cash img" />
+      </div>
+      <div className={styles.rightPage}>
+        <div className={styles.form}>
+          <input
+            type="number"
+            name="moedaReal"
+            id="real"
+            placeholder="Insira a quantia em reais"
+          />
+          <p>Cotacao atual do dolar: "Inserir variavel de cotacao"</p>
+          <input type="button" value="Converter" />
         </div>
-        <div className={styles.rightPage}>
-            <div className={styles.form}>
-                <input type="number" name="moedaReal" id="real" placeholder='Insira a quantia em reais'/>
-                <label htmlFor="moedaDolar" className='yep'> Cotacao atual do dolar: "Inserir variavel de cotacao" </label>
-                <input type="button" value="Converter"/>
-            </div>
-        </div>
+        <div className={styles.res} id="res">Tantos reais valem 594 dolares</div>
+      </div>
     </div>
   )
 }
