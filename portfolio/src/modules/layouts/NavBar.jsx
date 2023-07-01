f325b5c85866132e8bce2be65582ab58d165c829
FabricioSobrinho <fabriciomails3@gmail.com>
2023-07-01 15:48:29 -0400

    ...

--- portfolio/src/modules/layouts/NavBar.jsx
+++ portfolio/src/modules/layouts/NavBar.jsx
@@ -1,6 +1,30 @@
 import styles from "./NavBar.module.css"
+import { GiHamburgerMenu } from "react-icons/gi"
+
+import { useRef, useEffect, useState } from "react"
 
 function NavBar() {
+  const [display, setDisplay] = useState(false); //State começa como falso = menu uculto
+  const responsivemenu = useRef(null); //Localiza o menu na dom
+
+  const activateMenu = () => {
+    setDisplay(!display); //Muda o valor de display para alternar a visibilidade do Menu
+  };
+
+  useEffect(() => {
+    if (responsivemenu.current) { //Essa funcao determina se o menu esta ativado ou nao
+      const estilo = window.getComputedStyle(responsivemenu.current); 
+      const displayValue = estilo.getPropertyValue("display");
+      setDisplay(displayValue === "flex");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (responsivemenu.current) {
+      responsivemenu.current.style.display = display ? "flex" : "none"; //Esse operador é o responsável por mudar a visibiilidade do menu
+    }
+  }, [display]);
+
   return (
     <div>
       <nav>
@@ -24,6 +48,25 @@ function NavBar() {
               <a href="#contactSection">Contato</a>
             </li>
           </ul>
+        </div>
+        <div className={styles.responsiveMenu} onClick={activateMenu}>
+          <GiHamburgerMenu />
+          <div className={styles.layersMenu} ref={responsivemenu}>
+            <ul>
+              <li>
+                <a href="#aboutSection">Sobre</a>
+              </li>
+              <li>
+                <a href="#skillsSection">Skills</a>
+              </li>
+              <li>
+                <a href="#projectSection">Projetos</a>
+              </li>
+              <li>
+                <a href="#contactSection">Contato</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
